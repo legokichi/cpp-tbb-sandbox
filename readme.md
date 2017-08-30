73aa283f03d1679426680100b2d402110f05f5cb
legokichi <legokichi@gmail.com>
2017-08-31 04:42:06 +0900

    use gcc7

--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 
 ```sh
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-7 g++-7
 sudo apt install cmake build-essential
 sudo apt install libtbb-dev
 sudo apt install libboost-all-dev
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/task_scheduler_init.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <vector>
+#include <list>
+#include <tuple>
+#include <optional>
+#include <variant>
 
 auto main(int argc, char* argv[])-> int {
     using namespace std;
